00dfc5882e072f53d3318d048fd7d5dd38ce15fa
jeffritoo <jeffryvdavid@gmail.com>
2021-06-27 19:11:04 -0700

    first commit

---
+++ App.js
@@ -0,0 +1,24 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+
+import * as firebase from 'firebase'
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import LandingScreen from "./components/auth/Landing"
+
+const Stack = createStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Landing">
+        <Stack.Screen name="Landing" component={LandingScreen} options={{ headerShown: false}} />
+      </Stack.Navigator>
+    
+    </NavigationContainer>
+  );
+}
+
+
